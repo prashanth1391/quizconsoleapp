27cb237e1a608b146d1da875ba873d93d23e9487
Prashanth <prashanthkumarcse@gmail.com>
2024-04-12 00:03:26 +0530

    Added additional classes

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -7,6 +7,16 @@ public class Question {
     private String opt4;
     private String answer;
 
+    public Question(int id, String question, String opt1, String opt2, String opt3, String opt4, String answer) {
+        this.id = id;
+        this.question = question;
+        this.opt1 = opt1;
+        this.opt2 = opt2;
+        this.opt3 = opt3;
+        this.opt4 = opt4;
+        this.answer = answer;
+    }
+
     public int getId() {
         return id;
     }
@@ -61,5 +71,18 @@ public class Question {
 
     public void setAnswer(String answer) {
         this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", question='" + question + '\'' +
+                ", opt1='" + opt1 + '\'' +
+                ", opt2='" + opt2 + '\'' +
+                ", opt3='" + opt3 + '\'' +
+                ", opt4='" + opt4 + '\'' +
+                ", answer='" + answer + '\'' +
+                '}';
     }
 }
--- src/main/java/QuestionService.java
+++ src/main/java/QuestionService.java
@@ -1,3 +1,16 @@
 public class QuestionService {
     Question[] questions = new Question[5];
+
+    public QuestionService() {
+        questions[0] = new Question(1,"question1","1","2","3","4","2");
+        questions[1] = new Question(2,"question2","1","2","3","4","3");
+        questions[2] = new Question(3,"question3","1","2","3","4","1");
+        questions[3] = new Question(4,"question4","1","2","3","4","4");
+    }
+
+    public void displayQuestions() {
+        for(Question question: questions){
+            System.out.println(question);
+        }
+    }
 }
